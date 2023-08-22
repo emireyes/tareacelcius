c66be83756756d01073acd58ffe17fe3ff8faf19
emireyes <141955808+emireyes@users.noreply.github.com>
2023-08-21 19:10:49 -0600

    Add files via upload

---
+++ tareaalgoritmos 2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+	int main () {
+		
+	float temperaturaFahrenheit;
+	float temperaturaCelsius;	
+	
+	float celsius= 18;			
+	float fahrenheit= 86;
+	temperaturaFahrenheit=(9/5.0)*celsius+32;
+	temperaturaCelsius=(fahrenheit-32.0)*(5.0/9.0);
+	
+	printf("temperaturaFahrenheit: %f \n",temperaturaFahrenheit);
+	printf("temperaturaCelsius %f \n",temperaturaCelsius);
+	return 0;
+		
+			
+		
+	}
+	
+	
+
+
+
+
